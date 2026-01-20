f45beadaafb318ec46ad9a9e4cfe686e92657162
resha-ad <adhikariresha@gmail.com>
2026-01-20 20:34:19 +0545

    updated index.ts

---
+++ src/dtos/user.dto.ts
@@ -0,0 +1,26 @@
+import z from "zod";
+import { UserSchema } from "../types/user.type";
+
+export const CreateUserDTO = UserSchema.pick({
+  firstName: true,
+  lastName: true,
+  email: true,
+  username: true,
+  password: true,
+})
+  .extend({
+    confirmPassword: z.string().min(8, "Confirm password must be at least 8 characters"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Password and confirmation do not match",
+    path: ["confirmPassword"],
+  });
+
+export type CreateUserDTO = z.infer<typeof CreateUserDTO>;
+
+export const LoginUserDTO = z.object({
+  email: z.string().email("Invalid email format"),
+  password: z.string().min(8, "Password must be at least 8 characters"),
+});
+
+export type LoginUserDTO = z.infer<typeof LoginUserDTO>;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import express, { Application, Request, Response } from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 import { connectToDB } from "./database/mongodb";
 import { PORT } from "./config";
 import authRoutes from "./routes/auth.route";
 
 const app: Application = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  })
+);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
