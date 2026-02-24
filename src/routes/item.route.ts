9df69a3bd5aa5c949cb0d0c3991ee1e1cfa7152c
resha-ad <adhikariresha@gmail.com>
2026-02-24 11:58:23 +0545

    created item route

---
+++ src/routes/item.route.ts
@@ -0,0 +1,18 @@
+// src/routes/item.route.ts
+import { Router } from 'express';
+import { ItemController } from '../controllers/item.controller';
+import { protect, adminOnly, optionalProtect } from '../middlewares/authorized.middleware';
+import { uploadMultipleImages } from '../middlewares/upload.middleware';
+
+const router = Router();
+const controller = new ItemController();
+
+router.get('/', optionalProtect, controller.getAllItems);      //optional auth
+router.get('/:id', optionalProtect, controller.getSingleItem);
+
+router.use(protect, adminOnly);  // only applies to POST/PUT/DELETE
+router.post('/', uploadMultipleImages, controller.createItem);
+router.put('/:id', uploadMultipleImages, controller.updateItem);
+router.delete('/:id', controller.deleteItem);
+
+export default router;
